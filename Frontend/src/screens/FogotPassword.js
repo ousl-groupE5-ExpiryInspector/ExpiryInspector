f0176050a30b3d46735835087ee425cbacb02818
Sahani13 <118657274+Sahani13@users.noreply.github.com>
2024-09-17 17:43:44 +0530

    forget password #53
    updated forget password code.

--- Frontend/src/screens/FogotPassword.js
+++ Frontend/src/screens/FogotPassword.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import {View, StyleSheet, Alert } from 'react-native';
 import InputFieldCom from '../components/inputField';
 import ButtonComponentAuth from '../components/ButtonComponentAuth';
 import ImageComponentAuth from '../components/ImageComponentAuth';
@@ -8,23 +8,29 @@ import BackgroundFlex from '../components/BackgroundFlex';
 
 
 export default function FogotPasswordScreen({ navigation }) {
+  // State to store the user's email input
   const [email, setEmail] = useState('');
   
+  // Function to handle the "Link Email" button click
   const handleFogetPasword = () => {
-    navigation.navigate('Dashboard');
+    // Display alert
+    Alert.alert(
+      "Email Sent",
+      "Please check your email",
+      [
+        // Action when "OK" is pressed
+        { text: "OK", onPress: () => navigation.navigate('Login') }
+      ]
+    );
   };
   
   return (
+    // Using custom background layout for the screen
     <BackgroundFlex>
-
-      
       <HeaderTextComponent title="Reset Password" />
-
       
       <View style={{margin:30}} ></View>
       <ImageComponentAuth source={require('../../assets/Reset_Pasword.png')}/>
-
-      
 
       <View style={{margin:40}}></View>
       <InputFieldCom 
@@ -41,6 +47,7 @@ export default function FogotPasswordScreen({ navigation }) {
   );
 }
 
+// Define the styles for the screen elements
 const styles = StyleSheet.create({
   rowContainer: {
     flexDirection: 'row',
