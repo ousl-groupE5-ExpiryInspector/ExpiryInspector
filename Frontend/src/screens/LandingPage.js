657db05f4fcaf33d85e98832e5b5054150338422
Thidas Kaveesha Senavirathna <thidaskaveesha@gmail.com>
2024-09-19 19:12:26 +0530

    Added landing page connection

--- Frontend/src/screens/LandingPage.js
+++ Frontend/src/screens/LandingPage.js
@@ -9,14 +9,21 @@ export default function LandingPage({ navigation }) {
     navigation.navigate('Login');
   };
 
+  const handleFAQ = () => {
+    // Navigate to FAQ screen
+    navigation.navigate('FAQ');
+  }
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={handleFAQ} style={styles.faqContainer}>
+        <Image source={require('../../assets/faqIcon.png')} style={styles.faq} />
+      </TouchableOpacity>
       <View style={styles.curvedRectangle} />
       <Image source={require('../../assets/Landing_image.jpg')} style={styles.image} />
-      
+
       <View style={styles.buttonContainer}>
-        <ButtonComponentAuth title={'Get Started'} onPress={handleLanding}/>
-      
+        <ButtonComponentAuth title={'Get Started'} onPress={handleLanding} />
       </View>
     </View>
   );
@@ -29,27 +36,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  faqContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 55,
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+    backgroundColor: '#ccc',
+    borderRadius: 8,
+    padding: 5,
+  },
+  faq: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+  },
   curvedRectangle: {
     position: 'absolute',
     width: '100%',
-    height: '69%', 
-    backgroundColor: '#cfb5b2', 
-    borderBottomRightRadius: 320, 
-    top: 0, 
+    height: '69%',
+    backgroundColor: '#cfb5b2',
+    borderBottomRightRadius: 320,
+    top: 0,
     zIndex: -1,
   },
   image: {
     width: '100%',
-    height: '66%', 
+    height: '66%',
     borderBottomRightRadius: 310,
     overflow: 'hidden',
     resizeMode: 'cover',
     marginTop: 0,
   },
   buttonContainer: {
-    width: '100%', 
+    width: '100%',
     alignItems: 'center',
-    marginBottom: 70, 
+    marginBottom: 70,
   },
   button: {
     width: 209,
